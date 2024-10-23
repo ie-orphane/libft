bf8148808eec23924ade7d8eeddf62b8b3e10343
Ilyass Elyatime <ielyatim@student.1337.ma>
2024-10-23 14:08:16 +0100

    add bzero, memset and memchr

---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ielyatim <ielyatim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 10:26:48 by ielyatim          #+#    #+#             */
+/*   Updated: 2024/10/23 10:47:13 by ielyatim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*ptr;
+    size_t i;
+
+    i = 0;
+	ptr = (unsigned char	*)s;
+	while (ptr[i] && i < n)
+	{
+		if (ptr[i] == c)
+			return (ptr + i);
+		i++;
+	}
+	return (0);
+}
