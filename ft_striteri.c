2dfc6f646ce9d11b04f7f780655495264e94d542
Ilyass Elyatime <ielyatim@student.1337.ma>
2024-10-31 15:08:31 +0100

    additions: protect striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: ielyatim <ielyatim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 16:11:56 by ielyatim          #+#    #+#             */
-/*   Updated: 2024/10/27 17:48:13 by ielyatim         ###   ########.fr       */
+/*   Updated: 2024/10/31 15:07:33 by ielyatim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,13 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	i;
 
-	i = 0;
-	while (s[i])
-	{
-		f(i, &s[i]);
-		i++;
+	if (s)
+	{	
+		i = 0;
+		while (s[i])
+		{
+			f(i, &s[i]);
+			i++;
+		}
 	}
 }
