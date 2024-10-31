9fbe51398ff6f47623ba6d3b152bb96fd9924285
Ilyass Elyatime <ielyatim@student.1337.ma>
2024-10-31 11:23:41 +0100

    libc: protect calloc from crashing

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ielyatim <ielyatim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 13:05:13 by ielyatim          #+#    #+#             */
-/*   Updated: 2024/10/24 13:17:44 by ielyatim         ###   ########.fr       */
+/*   Updated: 2024/10/31 11:22:08 by ielyatim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	return (ft_memset(malloc(count * size), 0, count * size));
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (ptr)
+		ft_memset(ptr, 0, count * size);
+	return (ptr);
 }
