a1f86cf082b36c2186558fe7869b074861537ddf
Ilyass Elyatime <ielyatim@student.1337.ma>
2024-10-31 14:36:12 +0100

    additions : fix split

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: ielyatim <ielyatim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 20:20:13 by ielyatim          #+#    #+#             */
-/*   Updated: 2024/10/26 21:42:16 by ielyatim         ###   ########.fr       */
+/*   Updated: 2024/10/31 14:34:52 by ielyatim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_count_words(char *str, char c)
+static int	ft_count_words(const char *str, char c)
 {
 	int	i;
 	int	words_count;
@@ -32,48 +32,64 @@ static int	ft_count_words(char *str, char c)
 	return (words_count);
 }
 
-static char	*ft_walloc(char *str, char c, int *i)
+static char	*ft_walloc(const char *str, char c, size_t *i)
 {
-	int		j;
+	size_t	j;
 	char	*dest;
 
 	j = 0;
 	while (str[*i + j] != '\0' && str[*i + j] != c)
 		j++;
 	dest = malloc(j + 1);
-	j = 0;
-	while (str[*i] != '\0' && str[*i] != c)
+	if (dest)
 	{
-		dest[j] = str[*i];
-		(*i)++;
-		j++;
+		j = 0;
+		while (str[*i] != '\0' && str[*i] != c)
+		{
+			dest[j] = str[*i];
+			(*i)++;
+			j++;
+		}
+		dest[j] = '\0';
 	}
-	dest[j] = '\0';
 	return (dest);
+}
+
+char	**ft_free(char **arr, size_t count)
+{
+	while (count > 0)
+	{
+		free(arr[count]);
+		count--;
+	}
+	return (NULL);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int		i;
-	int		k;
+	size_t	i;
+	size_t	k;
 	char	**arr;
-	char	*str;
-	int		str_len;
 
-	str = (char *)s;
-	str_len = ft_strlen(str);
-	arr = malloc(sizeof(char *) * (ft_count_words(str, c) + 1));
-	i = 0;
-	k = 0;
-	while (i < str_len)
+	if (!s)
+		return (NULL);
+	arr = malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
+	if (arr)
 	{
-		if (str[i] != c)
+		i = 0;
+		k = 0;
+		while (i < ft_strlen(s))
 		{
-			arr[k] = ft_walloc(str, c, &i);
-			k++;
+			if (s[i] != c)
+			{
+				arr[k] = ft_walloc(s, c, &i);
+				if (!arr[k])
+					return (ft_free(arr, k));
+				k++;
+			}
+			i++;
 		}
-		i++;
+		arr[k] = NULL;
 	}
-	arr[k] = NULL;
 	return (arr);
 }
