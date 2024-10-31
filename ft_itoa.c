4aaf9236fad025541571521db2adea77c1fc12e1
Ilyass Elyatime <ielyatim@student.1337.ma>
2024-10-31 14:50:42 +0100

    additions: protect itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: ielyatim <ielyatim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 21:47:14 by ielyatim          #+#    #+#             */
-/*   Updated: 2024/10/26 21:53:46 by ielyatim         ###   ########.fr       */
+/*   Updated: 2024/10/31 14:50:06 by ielyatim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,28 +37,25 @@ char	*ft_itoa(int n)
 	long	nbr;
 	char	*str;
 	int		size;
-	int		end;
 
 	nbr = (long)n;
-	size = ft_size(nbr);
-	str = malloc(size);
-	end = 0;
 	if (nbr == 0)
+		return (ft_strdup("0"));
+	size = ft_size(nbr);
+	str = malloc(size + 1);
+	if (str)
 	{
-		end = 2;
-		str[0] = '0';
-	}
-	if (nbr < 0)
-	{
-		str[0] = '-';
-		end = 1;
-		nbr *= -1;
-	}
-	str[size] = '\0';
-	while (size > end)
-	{
-		str[--size] = nbr % 10 + 48;
-		nbr /= 10;
+		if (nbr < 0)
+		{
+			str[0] = '-';
+			nbr *= -1;
+		}
+		str[size] = '\0';
+		while (nbr > 0)
+		{
+			str[--size] = nbr % 10 + 48;
+			nbr /= 10;
+		}
 	}
 	return (str);
 }
