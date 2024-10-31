da60793b7ca3b29b4aa8a3f8fd9a8efadda55f9b
Ilyass Elyatime <ielyatim@student.1337.ma>
2024-10-31 08:34:41 +0100

    libc: make strncmp compatible with diffrent chars

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ielyatim <ielyatim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 14:46:13 by ielyatim          #+#    #+#             */
-/*   Updated: 2024/10/22 17:03:59 by ielyatim         ###   ########.fr       */
+/*   Updated: 2024/10/31 08:32:05 by ielyatim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned  char)s1[i] - (unsigned  char)s2[i]);
 }
