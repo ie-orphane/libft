c77a4694a482e5e3f6b4b18d276dae0cc2f1ec88
Ilyass Elyatime <ielyatim@student.1337.ma>
2024-10-31 12:44:48 +0100

    additions: fix strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,12 +6,11 @@
 /*   By: ielyatim <ielyatim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 09:58:54 by ielyatim          #+#    #+#             */
-/*   Updated: 2024/10/25 21:20:27 by ielyatim         ###   ########.fr       */
+/*   Updated: 2024/10/31 12:43:54 by ielyatim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 static int	ft_instr(const char c, const char *s)
 {
@@ -31,20 +30,25 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
 	size_t	j;
-	size_t	str_size;
+	size_t	s1_len;
 	char	*str;
 
-	str_size = 0;
+	if (!s1 || !set)
+		return (NULL);
 	i = 0;
 	while (s1[i] && ft_instr(s1[i], set) == 1)
 		i++;
-	while (s1[i + str_size] && ft_instr(s1[i + str_size], set) == 0)
-		str_size++;
-	str = malloc(str_size + 1);
+	j = ft_strlen(s1);
+	while (s1[j - 1] && ft_instr(s1[j - 1], set) == 1)
+		j--;
+	if (j < i)
+		return (ft_strdup(""));
+	s1_len = j - i;
+	str = malloc(s1_len + 1);
 	if (str)
 	{
 		j = 0;
-		while (j < str_size)
+		while (j < s1_len)
 			str[j++] = s1[i++];
 		str[j] = '\0';
 	}
