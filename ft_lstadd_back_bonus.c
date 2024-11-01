0763ade97d4ba89b1040bf7159f3f8ebc5fafa22
Ilyass Elyatime <ielyatim@student.1337.ma>
2024-11-01 10:53:46 +0100

    bonus: prevent making new->next set to NULL in lstadd_back

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ielyatim <ielyatim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 11:49:59 by ielyatim          #+#    #+#             */
-/*   Updated: 2024/10/31 17:57:53 by ielyatim         ###   ########.fr       */
+/*   Updated: 2024/11/01 10:46:49 by ielyatim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,4 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 		*lst = new;
 	else
 		ft_lstlast(*lst)->next = new;
-	new->next = NULL;
 }
