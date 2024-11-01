607308fddf3d2997c4ffc603e8f0e34a91bbb1b4
Ilyass Elyatime <ielyatim@student.1337.ma>
2024-11-01 09:50:18 +0100

    libc: cast c to unsigned char in strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: ielyatim <ielyatim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 11:22:15 by ielyatim          #+#    #+#             */
-/*   Updated: 2024/10/22 14:17:27 by ielyatim         ###   ########.fr       */
+/*   Updated: 2024/11/01 09:49:47 by ielyatim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@ char	*ft_strchr(const char *s, int c)
 {
 	while (*s != 0)
 	{
-		if (*s == c)
+		if (*s == (unsigned char)c)
 			return ((char *)s);
 		s++;
 	}
-	if (*s == c)
+	if (*s == (unsigned char)c)
 		return ((char *)s);
 	return (0);
 }
