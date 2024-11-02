5692bb28899969dd2cc9ca155e0b904f59bcfd29
Ilyass Elyatime <ielyatim@student.1337.ma>
2024-11-02 18:39:26 +0100

    bonus: avoid memory leaks in lstmap

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ielyatim <ielyatim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 08:41:25 by ielyatim          #+#    #+#             */
-/*   Updated: 2024/10/29 12:03:31 by ielyatim         ###   ########.fr       */
+/*   Updated: 2024/11/02 18:05:32 by ielyatim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,25 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*start_lst;
 	t_list	*new_lst;
+	void	*new_content;
 
 	start_lst = NULL;
 	while (lst)
 	{
-		new_lst = ft_lstnew(f(lst->content));
+		new_content = f(lst->content);
+		if (!new_content)
+		{
+			ft_lstclear(&start_lst, del);
+			return (NULL);
+		}
+		new_lst = ft_lstnew(new_content);
 		if (!new_lst)
 		{
-			ft_lstclear(&new_lst, del);
-			continue ;
+			del(new_content);
+			ft_lstclear(&start_lst, del);
+			return (NULL);
 		}
-		if (!start_lst)
-			start_lst = new_lst;
-		else
-			ft_lstadd_back(&start_lst, new_lst);
+		ft_lstadd_back(&start_lst, new_lst);
 		lst = lst->next;
 	}
 	return (start_lst);
