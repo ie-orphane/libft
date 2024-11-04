5993d1c41bec49fc005e27ae2e7dc13022648ea5
Ilyass Elyatime <ielyatim@student.1337.ma>
2024-11-04 10:37:45 +0100

    bonus: protect lstmap from NULL f, and NULL del

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ielyatim <ielyatim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 08:41:25 by ielyatim          #+#    #+#             */
-/*   Updated: 2024/11/02 18:05:32 by ielyatim         ###   ########.fr       */
+/*   Updated: 2024/11/04 10:37:16 by ielyatim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	void	*new_content;
 
 	start_lst = NULL;
-	while (lst)
+	while (lst && f && del)
 	{
 		new_content = f(lst->content);
 		if (!new_content)
