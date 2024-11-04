4659557a5fce2b1870e9638acd020dc78d8c7f60
Ilyass Elyatime <ielyatim@student.1337.ma>
2024-11-04 10:32:33 +0100

    bonus: protect lstaddd_front from NULL lst

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ielyatim <ielyatim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 10:40:37 by ielyatim          #+#    #+#             */
-/*   Updated: 2024/10/31 15:01:19 by ielyatim         ###   ########.fr       */
+/*   Updated: 2024/11/04 10:31:21 by ielyatim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
+	if (lst)
+		return ;
 	new->next = *lst;
 	*lst = new;
 }
