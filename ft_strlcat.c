1de6ca67f329876cfac3968c887db2027e29aa1f
Ilyass Elyatime <ielyatim@student.1337.ma>
2024-11-05 10:04:01 +0100

    libc: handle dst=NULL and dstsize=0 in strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ielyatim <ielyatim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 08:36:45 by ielyatim          #+#    #+#             */
-/*   Updated: 2024/11/01 09:50:49 by ielyatim         ###   ########.fr       */
+/*   Updated: 2024/11/04 16:08:38 by ielyatim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	dst_len;
 	size_t	src_len;
 
-	if (!dst && size == 0)
-		return (size);
 	src_len = ft_strlen(src);
+	if (!dst && size == 0)
+		return (src_len);
 	dst_len = ft_strlen(dst);
-	j = 0;
-	i = dst_len;
 	if (dst_len >= size)
 		return (size + src_len);
-	while (src[j] != '\0' && j < (size - dst_len - 1))
+	j = 0;
+	i = dst_len;
+	while (src[j] && j < (size - dst_len - 1))
 		dst[i++] = src[j++];
 	dst[i] = '\0';
 	return (src_len + dst_len);
