232e53b600fbdb3c6155ccee4f8e514edebbab79
Ilyass Elyatime <ielyatim@student.1337.ma>
2024-11-05 10:14:32 +0100

    libc: convert c to char instead of unsigned char in strchr and strrchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: ielyatim <ielyatim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 11:22:15 by ielyatim          #+#    #+#             */
-/*   Updated: 2024/11/01 09:49:47 by ielyatim         ###   ########.fr       */
+/*   Updated: 2024/11/05 10:11:08 by ielyatim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@ char	*ft_strchr(const char *s, int c)
 {
 	while (*s != 0)
 	{
-		if (*s == (unsigned char)c)
+		if (*s == (char)c)
 			return ((char *)s);
 		s++;
 	}
-	if (*s == (unsigned char)c)
+	if (*s == (char)c)
 		return ((char *)s);
 	return (0);
 }
